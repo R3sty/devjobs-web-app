732de8f7816fd07294f1d789c496de731780dc2c
RyanSuda-Code <sudaryan49@yahoo.com>
2026-03-25 18:39:13 +0900

    Update: Removed My notes and renamed the parameter to be more appropriate and consistent with the task.

--- renderJobs.js
+++ renderJobs.js
@@ -1,21 +1,21 @@
 const rendJob = document.querySelector('.job-container');
 
-fetch('./data.json') // request or grab a file called data.json
-  .then(res => res.json()) // converts the response(res) which is the file you fetched into a usable Javascript data
-  .then(data => { // data is now a JS object/array created from json files
-    data.forEach(post => { // loop throught tehe array/object one item at a time. For each object call it post and run this function. post = one single jopb object. 
-      rendJob.insertAdjacentHTML('beforeend', // evvery time the loop runs it creates a job card and insert it in rendJob which is the job-contaiener. with beforeend we add each new job inside the element at the end of its last child
+fetch('./data.json')
+  .then(res => res.json()) 
+  .then(data => { 
+    data.forEach(jobs => { 
+      rendJob.insertAdjacentHTML('beforeend',
         ` <div class="job-card">
-          <div class="logo-container" style="background-color: ${post.logoBackground};">
+          <div class="logo-container" style="background-color: ${jobs.logoBackground};">
             <img
-              src="${post.logo}"
+              src="${jobs.logo}"
             />
           </div>
           <section class="job-info">
-            <p>${post.postedAt} · ${post.contract}</p>
-            <h3>${post.position}</h3>
-            <p class="comp-name">${post.company}</p>
-            <span class="highlight">${post.location}</span>
+            <p>${jobs.postedAt} · ${jobs.contract}</p>
+            <h3>${jobs.position}</h3>
+            <p class="comp-name">${jobs.company}</p>
+            <span class="highlight">${jobs.location}</span>
           </section>
         </div>`
       );
