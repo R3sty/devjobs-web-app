71c8afa8259b43627ab87641564fcc2d2c1ca770
RyanSuda-Code <sudaryan49@yahoo.com>
2026-04-02 22:09:51 +0900

    Added a JS that would prevent the page to refresh when the submit button is activated.

---
+++ script.js
@@ -0,0 +1,5 @@
+const form = document.querySelector('.search-bar');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();// prevents the page to refresh when the form is submitted
+});
